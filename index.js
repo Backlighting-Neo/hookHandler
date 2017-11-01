2376c07b049c3a267208a201afb07a68adb2e536
KevinTang <tjtzc@126.com>
2017-11-01 21:49:51 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+const exec = require('child_process').exec; 
+const asyncExec = command => new Promise((resolve, reject) => {
+  exec(command, (err, stdout, stderr) => {
+    if(!err) resolve();
+    else reject();
+  })
+})
+
+
+
+router.get('/updateGitRepo', async (context, next) => {
+  await asyncExec('cd /var/www/html/schoolcms && git pull');
+  context.body = 'ok';
+});
+
+app.use(router.routes())
+.use(router.allowedMethods());
+
+app.listen(8981);
